888aa83d4b01e426ff8af701340b6362db83d28b
Dianna237 <mbiayambadianna@gmail.com>
2023-06-06 19:03:26 +0100

    update component

--- react-app/my-app/src/App.js
+++ react-app/my-app/src/App.js
@@ -3,12 +3,24 @@ import Header from './component/header/Header'
 import './App.css';
 import Contact from './component/contact/Contact';
 import Navbar from './component/navbar/Navbar';
+import Footer from './component/footer/Footer';
+import About from './component/about/About';
+import Services from './component/services/Services';
+import Testimonial from './component/testimonial/Testimonial';
+import Experience from './component/experience/Experience';
+import Portfolio from './component/portfolio/Portfolio';
 const App = ()=> {
   return (
     <div>
-      <Header />
-      <Contact />
       <Navbar />
+      <Header />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>   
   );
 }
